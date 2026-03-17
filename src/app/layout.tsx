0c83a75aa653b75800e0a88fa1071373d531f4a9
Mikaiel Danielian <midan888@gmail.com>
2026-03-17 10:15:32 +0300

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,13 @@ export const metadata: Metadata = {
     images: ["/opengraph-image"],
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon.ico", sizes: "48x48" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
     apple: "/apple-touch-icon.png",
   },
 };
